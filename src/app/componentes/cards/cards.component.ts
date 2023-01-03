028f5f9a86ac92a81e7a2e6dedcb225724f98417
Roman-code-web <selenerg.275@gmail.com>
2023-01-03 14:39:56 -0500

    logica terminada

--- src/app/componentes/cards/cards.component.ts
+++ src/app/componentes/cards/cards.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent {
-
+  mostrarDiv!:boolean;
+  nombre="Activar";
+  mostar( check:HTMLInputElement){
+    this.mostrarDiv=true;
+    this.nombre="Desactivar"
+    if(check.checked==false){
+      this.mostrarDiv=false;
+      this.nombre="Activar";
+    }
+  }
 }
