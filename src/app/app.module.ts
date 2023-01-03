b737b9a0e4d8b3b5ebf06901d2bd4248a1b4d34e
Roman-code-web <selenerg.275@gmail.com>
2023-01-03 14:16:58 -0500

    creacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentesModule } from './componentes/componentes.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ComponentesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
